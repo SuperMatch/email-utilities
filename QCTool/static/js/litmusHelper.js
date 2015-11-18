0ff20c5bfcbaebee62e648e70df01f1b848c2b0a
Yunxiang Chai <cyxjimmy@gmail.com>
2015-11-18 12:21:05 +0800

    Update Litmus Helper Rule

--- QCTool/static/js/litmusHelper.js
+++ QCTool/static/js/litmusHelper.js
@@ -5,18 +5,18 @@ var linker = angular.module('Linker', []).config(function($interpolateProvider) 
 
 
 linker.factory('Data', function () {
-    var Data = {}
+    var Data = {};
     Data.warningDisplay = "show";
     Data.listDisplay = "hidden";
     Data.litmusURL = "";
     Data.config = [
         {
-            name: "Apple Mail 6",
-            suffix: "appmail6"
+            name: "Apple Mail 7",
+            suffix: "appmail7"
         },
         {
-            name: "Lotus Notes 6.5",
-            suffix: "notes6"
+            name: "Apple Mail 8",
+            suffix: "appmail8"
         },
         {
             name: "Lotus Notes 7",
@@ -29,6 +29,10 @@ linker.factory('Data', function () {
         {
             name: "Lotus Notes 8.5",
             suffix: "notes85"
+        },
+        {
+            name: "IBM Notes 9",
+            suffix: "note9"
         },
         {
             name: "Outlook 2000",
@@ -59,15 +63,19 @@ linker.factory('Data', function () {
             suffix: "ol2013"
         },
         {
-            name: "Thunderbird latest",
+            name: "Outlook 2013 120 DPI",
+            suffix: "ol2013dpi120"
+        },
+        {
+            name: "Outlook 2016",
+            suffix: "ol2015"
+        },
+        {
+            name: "Thunderbird 38",
             suffix: "thunderbirdlatest"
         },
         {
-            name: "Android 2.3",
-            suffix: "android22"
-        },
-        {
-            name: "Android 4.2",
+            name: "Android 4.4",
             suffix: "android4"
         },
         {
@@ -75,32 +83,12 @@ linker.factory('Data', function () {
             suffix: "androidgmailapp"
         },
         {
-            name: "Black Berry 4 OS",
-            suffix: "blackberry8900"
-        },
-        {
-            name: "Black Berry 5 OS",
-            suffix: "blackberryhtml"
-        },
-        {
-            name: "iPhone 5s",
+            name: "iPhone 5s (iOS 7)",
             suffix: "iphone5s"
         },
         {
-            name: "iPad (Retina)",
-            suffix: "ipad"
-        },
-        {
-            name: "iPad Mini",
-            suffix: "ipadmini"
-        },
-        {
-            name: "iPhone 4s",
-            suffix: "iphone4"
-        },
-        {
-            name: "iPhone 5",
-            suffix: "iphone5"
+            name: "iPhone 5s (iOS 8)",
+            suffix: "iphone5sios8"
         },
         {
             name: "iPhone 6",
@@ -111,8 +99,20 @@ linker.factory('Data', function () {
             suffix: "iphone6plus"
         },
         {
-            name: "Windows Phone 8",
-            suffix: "windowsphone8"
+            name: "iPhone 6s",
+            suffix: "iphone6s"
+        },
+        {
+            name: "iPhone 6s Plus",
+            suffix: "iphone6splus"
+        },
+        {
+            name: "iPad (Retina)",
+            suffix: "ipad"
+        },
+        {
+            name: "iPad Mini",
+            suffix: "ipadmini"
         },
         {
             name: "AOL Mail (Explorer)",
@@ -137,6 +137,30 @@ linker.factory('Data', function () {
         {
             name: "Gmail (Chrome)",
             suffix: "chromegmailnew"
+        },
+        {
+            name: "Google Apps (Explorer)",
+            suffix: "googleapps"
+        },
+        {
+            name: "Google Apps (Chrome)",
+            suffix: "chromegoogleapps"
+        },
+        {
+            name: "Google Apps (Firefox)",
+            suffix: "ffgoogleapps"
+        },
+        {
+            name: "Office 365 (Explorer)",
+            suffix: "office365"
+        },
+        {
+            name: "Office 365 (Firefox)",
+            suffix: "ffoffice365"
+        },
+        {
+            name: "Office 365 (Chrome)",
+            suffix: "chromeoffice365"
         },
         {
             name: "Outlook.com (Explorer)",
@@ -171,18 +195,19 @@ function renderLink($scope, Data){
     $scope.data = Data;
     $scope.config = Data.config;
     $scope.renderLink = function (device) {
-        var renderURL = Data.stripURL + "results#";
-        return baseUrl = renderURL + device.suffix + "-full_on";
+        //var renderURL = Data.stripURL + "results#";
+        return baseUrl = Data.litmusURL + "#" + device.suffix;
     }
 }
 
 function linkInput($scope, Data){
     $scope.data = Data;
     $scope.change = function () {
-        var regex = /(https:\/\/litmus.com\/tests\/\d+\/versions\/\d+\/).*/
-        var result = Data.litmusURL.match(regex);
-        if(result){
-            Data.stripURL = result[1]
+        var regex1 = /(^http(s)?:\/\/litmus.com\/checklist\/tests\/\d+$)/;
+        var result1 = Data.litmusURL.match(regex1);
+        var regex2 = /(^http(s)?:\/\/litmus.com\/pub\/\w+$)/;
+        var result2 = Data.litmusURL.match(regex2);
+        if(result1 || result2){
             Data.warningDisplay = "hidden";
             Data.listDisplay = "show";
         }else{
